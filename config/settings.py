2ffd1b2dd4b6ad38dbe798a2716d5adf4ec6eb2a
Daniel Santiago Vera Arias <dsveraa.ing@gmail.com>
2026-09-16 12:09:52 -0500

    Refactor: Anexamos correccion de subida de documentos a una carpeta en especifico.

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# CONFIGURACION PARA CARPETA DE MEDIA
+MEDIA_URL = "/media/"
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
